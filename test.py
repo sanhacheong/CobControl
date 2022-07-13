3648263f9527d86cd82232fa052a7130aeeb9503
Sanha Cheong <sanha@rddev111.slac.stanford.edu>
2022-07-13 10:35:24 -0700

    Updating .gitignore

--- test.py
+++ test.py
@@ -12,13 +12,14 @@ def main(command,
          config_json="./cob_host_config.json",
          on_off_bits="111011111"):
 
+    print(f"Opening COB config file: {config_json}")
     with open(config_json, 'r') as f:
         host_config = json.load(f)
+    print(f"Successfully loaded COB config file!!!")
 
+    print(f"Configuring parallel SSH links to RCE's...")
     if len(on_off_bits) != len(host_config[cob_name]["RCEList"]):
         raise ValueError(f"Length of on_off_bits ({len(on_off_bits)}) does NOT match # of RCE's in config ({len(host_config[cob_name]['RCEList'])})!")
-        # raise ValueError(f"Length of on_off_bits does NOT match # of RCE's in config!")
-
     rce_hosts = []
     rce_configs = []
     for i, rce in enumerate(host_config[cob_name]["RCEList"]):
@@ -29,11 +30,13 @@ def main(command,
             )
 
     client = ParallelSSHClient(rce_hosts, host_config=rce_configs)
+    print("Successfully configured SSH links!")
+    print(f"Commands will be sent to: {rce_hosts}")
 
     output = client.run_command(command)
 
     for host_output in output:
-        print(host_output)
+        # print(host_output)
         for line in host_output.stdout:
             print(line)
 
