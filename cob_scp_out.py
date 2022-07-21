d651629bb57e1465ba3eaa2c0e9cfe4993db8cda
Sanha Cheong <sanha@rddev111.slac.stanford.edu>
2022-07-20 17:00:51 -0700

    Implemented scp functionality, changed script name

---
+++ cob_scp_out.py
@@ -0,0 +1,55 @@
+import json
+import re
+from pprint import pprint
+
+import fire
+import commands
+
+from pssh.clients import ParallelSSHClient
+from pssh.config import HostConfig
+from pssh.utils import enable_logger, logger
+from gevent import joinall
+
+def main(local_path, remote_path,
+         cob_name="COB2",
+         config_json="./cob_host_config.json",
+         target_machine="dpm"):
+
+    enable_logger(logger)
+
+    print("Opening COB config file...")
+    print(f"COB config file: {config_json}")
+    with open(config_json, 'r') as f:
+        config_dict = json.load(f)
+    print(f"Successfully loaded COB config file!!!\n")
+
+    rce_list = re.findall(f"{target_machine}\S*",
+                          ' '.join([rce for rce in config_dict[cob_name].keys()]),
+                          flags=re.IGNORECASE)
+
+    rce_hosts = []
+    rce_configs = []
+    for rce_name in rce_list:
+        rce_hosts.append(config_dict[cob_name][rce_name]["Address"])
+        rce_configs.append(
+            HostConfig(user=config_dict[cob_name][rce_name]["DefaultUser"],
+                       password=config_dict[cob_name][rce_name]["DefaultPassword"])
+        )
+
+    print("Setting up SSH hosts...")
+    print(rce_hosts)
+    client = ParallelSSHClient(rce_hosts, host_config=rce_configs)
+    print("Successfully configured SSH links!!!\n")
+
+    print("Sending the local file(s) to all hosts...")
+    print(f"Hosts: {rce_hosts}")
+    print(f"Local file path: {local_path}")
+    output = client.scp_send(local_path, remote_path,
+                             recurse=True)
+    joinall(output, raise_error=True)
+    print("Files sent without errors!!!\n")
+
+    print("Exiting script successfully!!!")
+
+if __name__ == '__main__':
+    fire.Fire(main)
